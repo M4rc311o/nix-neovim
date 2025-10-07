28b5af140d2b08744126faf7fe4c8a8a6677552b
M4rc3110 <git@m4rc3110.foo>
2025-10-07 13:23:45 +0200

    Add a little suffed markdown preview support

--- lua/m4rc3110/plugins/markdown-preview.lua
+++ lua/m4rc3110/plugins/markdown-preview.lua
@@ -4,7 +4,13 @@ return {
 		cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
 		ft = "markdown",
 		keys = {
-			{ "<leader>mp", "<cmd>MarkdownPreview <CR>", mode = { "n" }, noremap = true, desc = "Markdown preview" },
+			{
+				"<leader>mp",
+				"<cmd>MarkdownPreview <CR>",
+				mode = { "n" },
+				noremap = true,
+				desc = "Markdown preview start",
+			},
 			{
 				"<leader>ms",
 				"<cmd>MarkdownPreviewStop <CR>",
@@ -20,8 +26,36 @@ return {
 				desc = "Markdown preview toggle",
 			},
 		},
-		-- before = function(plugin)
-		-- 	vim.g.mkdp_auto_close = 0
-		-- end,
+		before = function(plugin)
+			-- vim.g.mkdp_auto_close = 0
+
+			vim.cmd([[
+                function! OpenMarkdownPreview(url) abort
+                    if executable('chromium')
+                        call jobstart(["chromium", "--new-window", "--disable-extensions", "--disable-sync", "--no-first-run", printf("--app=%s", a:url)])
+                    else
+                        call jobstart(["xdg-open", a:url])
+                    endif
+                endfunction
+            ]])
+
+			vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
+
+			-- function _G.OpenBrowser(url)
+			-- 	vim.notify("Markdown preview URL: " .. url)
+			-- 	if vim.fn.executable("chromium") then
+			-- 		vim.fn.system({
+			-- 			"chromium",
+			-- 			"--app=" .. url,
+			-- 			"--new-window",
+			-- 			"--disable-extensions",
+			-- 			"--disable-sync",
+			-- 			"--no-first-run",
+			-- 		})
+			-- 	else
+			-- 		vim.fn.system({ "xdg-open", url })
+			-- 	end
+			-- end
+		end,
 	},
 }
